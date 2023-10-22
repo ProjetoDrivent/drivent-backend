d3d37267579745b6dd13e1dd1c843cdf6623190f
Caio Neme <caiocesarneme@gmail.com>
2023-10-22 12:01:12 -0300

    Feat(Prisma):add Seed for TicktsTypes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import dayjs from "dayjs";
+import { PrismaClient } from '@prisma/client';
+import dayjs from 'dayjs';
 const prisma = new PrismaClient();
 
 async function main() {
@@ -7,13 +7,39 @@ async function main() {
   if (!event) {
     event = await prisma.event.create({
       data: {
-        title: "Driven.t",
-        logoImageUrl: "https://files.driven.com.br/images/logo-rounded.png",
-        backgroundImageUrl: "linear-gradient(to right, #FA4098, #FFD77F)",
+        title: 'Driven.t',
+        logoImageUrl: 'https://files.driven.com.br/images/logo-rounded.png',
+        backgroundImageUrl: 'linear-gradient(to right, #FA4098, #FFD77F)',
         startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, "days").toDate(),
+        endsAt: dayjs().add(21, 'days').toDate(),
       },
     });
+
+    let ticketType = await prisma.ticketType.findMany();
+    if (ticketType.length === 0) {
+      await prisma.ticketType.createMany({
+        data: [
+          {
+            name: 'Online',
+            price: 100,
+            isRemote: true,
+            includesHotel: false,
+          },
+          {
+            name: 'Presencial',
+            price: 250,
+            isRemote: false,
+            includesHotel: false,
+          },
+          {
+            name: 'Presencial+Hotel',
+            price: 600,
+            isRemote: false,
+            includesHotel: true,
+          },
+        ],
+      });
+    }
   }
 
   console.log({ event });
