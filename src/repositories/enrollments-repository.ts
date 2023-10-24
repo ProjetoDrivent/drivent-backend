427d4a739a60a9a30cc14e148d98a8a027394083
gustavohubnerc <hubnergc@gmail.com>
2023-10-24 15:20:32 -0300

    feat: transactions

--- src/repositories/enrollments-repository.ts
+++ src/repositories/enrollments-repository.ts
@@ -31,3 +31,37 @@ export const enrollmentRepository = {
   findWithAddressByUserId,
   upsert,
 };
+
+/* async function upsert(
+  userId: number,
+  createdEnrollment: CreateEnrollmentParams,
+  updatedEnrollment: UpdateEnrollmentParams,
+) {
+  const transactionResult = await prisma.$transaction(async (prisma) => {
+    const enrollment = await prisma.enrollment.upsert({
+      where: {
+        userId,
+      },
+      create: createdEnrollment,
+      update: updatedEnrollment,
+    });
+
+    const address = await prisma.address.upsert({
+      where: {
+        enrollmentId: enrollment.id,
+      },
+      create: {
+        ...createdEnrollment.Address,
+        enrollmentId: enrollment.id,
+      },
+      update: {
+        ...updatedEnrollment.Address,
+        enrollmentId: enrollment.id,
+      },
+    });
+
+    return { enrollment, address };
+  });
+
+  return transactionResult;
+} */
